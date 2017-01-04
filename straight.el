ffac08ef6be3da9289c489d814cdab8781b01c80
Radon Rosborough <radon.neon@gmail.com>
2017-01-03 22:21:52 -0700

    Add package template

---
+++ straight.el
@@ -0,0 +1,24 @@
+;;; straight.el --- The straightforward package manager.
+
+;; Copyright (C) 2017 Radon Rosborough
+
+;; Author: Radon Rosborough <radon.neon@gmail.com>
+;; Homepage: https://github.com/raxod502/straight.el
+;; Keywords: extensions
+;; Created: 1 Jan 2017
+
+;;; Commentary:
+
+;; Please see https://github.com/raxod502/straight.el for more
+;; information.
+
+;;; Code:
+
+;; To see the outline of this file, run M-x occur with a query of four
+;; semicolons followed by a space.
+
+;; Code goes here.
+
+(provide 'straight)
+
+;;; straight.el ends here
