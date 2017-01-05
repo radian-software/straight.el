0a98d1aa86718c910c19336c203cd7e93ef1c528
Radon Rosborough <radon.neon@gmail.com>
2017-01-04 17:01:04 -0700

    Add some wishful thinking

--- straight.el
+++ straight.el
@@ -17,6 +17,42 @@
 ;; To see the outline of this file, run M-x occur with a query of four
 ;; semicolons followed by a space.
 
+;; Wishful thinking API.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Selecting packages
+
+(defun straight-select-installed-package ()
+  (message "FIXME: actually select an installed package"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Building packages
+
+(defun straight-symlink-package (package)
+  (interactive (list (straight-select-installed-package)))
+  (message "FIXME: actually symlink package"))
+
+(defun straight-generate-package-autoloads (package)
+  (interactive (list (straight-select-installed-package)))
+  (message "FIXME: actually generate package autoloads"))
+
+(defun straight-byte-compile-package (package)
+  (interactive (list (straight-select-installed-package)))
+  (message "FIXME: actually byte-compile package"))
+
+(defun straight-build-package (package)
+  (interactive (list (straight-select-installed-package)))
+  (straight-symlink-package package)
+  (straight-generate-package-autoloads package)
+  (straight-byte-compile-package package))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Activating packages
+
+(defun straight-activate-package-autoloads (package)
+  (interactive (list (straight-select-installed-package)))
+  (message "FIXME: actually activate package autoloads"))
+
 ;; Code goes here.
 
 (provide 'straight)
