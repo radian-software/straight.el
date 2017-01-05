0da45d76467d3a96531ac07ce342af17a1e7276f
Radon Rosborough <radon.neon@gmail.com>
2017-01-05 09:37:11 -0700

    Add more plans

--- straight.el
+++ straight.el
@@ -17,31 +17,47 @@
 ;; To see the outline of this file, run M-x occur with a query of four
 ;; semicolons followed by a space.
 
-;; Wishful thinking API.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Computing filesystem paths
+
+(defvar straight-directory "straight")
+
+(defun straight-directory ()
+  (concat user-emacs-directory straight-directory))
+
+(defvar straight-repo-directory "repo")
+
+(defun straight-repo-directory ()
+  (concat (straight-directory) straight-repo-directory))
+
+(defvar straight-build-directory "build")
+
+(defun straight-build-directory ()
+  (concat (straight-directory) straight-build-directory))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Selecting packages
 
 (defun straight-select-installed-package ()
-  (message "FIXME: actually select an installed package"))
+  (completing-read
+   "Select package: "
+   (directory-files (straight-build-directory) nil "^[^.]$")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Checking whether packages need to be rebuilt
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Building packages
 
-(defun straight-symlink-package (package)
-  (interactive (list (straight-select-installed-package)))
-  (message "FIXME: actually symlink package"))
-
 (defun straight-generate-package-autoloads (package)
-  (interactive (list (straight-select-installed-package)))
   (message "FIXME: actually generate package autoloads"))
 
 (defun straight-byte-compile-package (package)
-  (interactive (list (straight-select-installed-package)))
   (message "FIXME: actually byte-compile package"))
 
 (defun straight-build-package (package)
-  (interactive (list (straight-select-installed-package)))
   (straight-symlink-package package)
   (straight-generate-package-autoloads package)
   (straight-byte-compile-package package))
@@ -53,7 +69,8 @@
   (interactive (list (straight-select-installed-package)))
   (message "FIXME: actually activate package autoloads"))
 
-;; Code goes here.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Closing remarks
 
 (provide 'straight)
 
