bd243ade8f2c1452aa64b77335c54348761a477d
Radon Rosborough <radon.neon@gmail.com>
2019-12-07 10:39:35 -0800

    Prevent symlink from being created inside srcdir

--- scripts/smoke-test.bash
+++ scripts/smoke-test.bash
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 mkdir -p "$HOME/.emacs.d/straight/repos/"
-ln -s "$PWD" "$HOME/.emacs.d/straight/repos/straight.el"
+ln -sf "$PWD" "$HOME/.emacs.d/straight/repos/straight.el"
 
 # We need to test with a package that supports Emacs 24.5 here.
 emacs --batch -l "$HOME/.emacs.d/straight/repos/straight.el/bootstrap.el" \
