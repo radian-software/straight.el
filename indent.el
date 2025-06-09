e7284e036f0336287fec64c9020c932b5eecc746
Radon Rosborough <radon@intuitiveexplanations.com>
2025-06-09 11:46:39 -0700

    Add more comments to indent.el

--- indent.el
+++ indent.el
@@ -1,5 +1,10 @@
 ;; You can load this file to make sure that forms from other packages
-;; are indented correctly.
+;; are indented correctly. It should cause the indentation to be the
+;; same as if you had actually loaded the packages that define these
+;; functions.
+;;
+;; Generally speaking, we use indentation rules from Emacs 30 to
+;; indent the straight.el source code.
 
 (put #'if-let 'lisp-indent-function 2)
 (put #'when-let 'lisp-indent-function 1)
