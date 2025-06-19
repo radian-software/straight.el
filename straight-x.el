eb74e0f3014c276f9605ae020d4e46def05ca212
Radon Rosborough <radon@intuitiveexplanations.com>
2025-06-19 12:11:12 -0700

    Display SSH prompts interactively DURING bootstrap

--- straight-x.el
+++ straight-x.el
@@ -152,7 +152,7 @@
   (let ((lockfile-path (straight--versions-lockfile 'pinned)))
     (with-temp-file lockfile-path
       (insert
-       (format "(%s)\n:delta\n"
+       (format "(%s)\n:epsilon\n"
                (mapconcat
                 (apply-partially #'format "%S")
                 straight-x-pinned-packages
